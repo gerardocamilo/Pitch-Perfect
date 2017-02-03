47382726fdb47b471228cc6c06143dbf933e877d
Gerardo Camilo <gerardo.camilo@gmail.com>
2017-02-02 22:00:16 -0400

    Removing spaces and adding access modifiers to make the code more swift-style-guide compliant.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -21,16 +21,16 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var stopButton: UIButton!
 
     // MARK: - Properties
-    var recordedAudioURL: URL!
-    var audioFile: AVAudioFile!
-    var audioEngine: AVAudioEngine!
-    var audioPlayerNode: AVAudioPlayerNode!
-    var stopTimer: Timer!
-    
+    internal var recordedAudioURL: URL!
+    internal var audioFile: AVAudioFile!
+    internal var audioEngine: AVAudioEngine!
+    internal var audioPlayerNode: AVAudioPlayerNode!
+    internal var stopTimer: Timer!
+
     enum ButtonType: Int {
         case slow = 0, fast, chipmunk, vader, echo, reverb
     }
-    
+
     /*
      // MARK: - playSoundForButton
      Base on the caller button determines what parameters should be sent to the audio player to get the expected result.
@@ -49,26 +49,24 @@ class PlaySoundsViewController: UIViewController {
             playSound(echo: true)
         case .reverb:
             playSound(reverb: true)
-            
         }
-        
+
         //Indicating to the UI that it should adapt to "playing" state.
         configureUI(.playing)
     }
-    
+
     @IBAction func stopButtonPressed(_sender: AnyObject) {
         stopAudio()
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupAudio()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         configureUI(.notPlaying)
     }
-    
+
   } //PlaySoundsViewController class end
--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 import AVFoundation
 
-class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
-    
+final class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+
     // MARK: Properties
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var startRecordingButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
-    var audioRecorder: AVAudioRecorder!
-    
+    private var audioRecorder: AVAudioRecorder!
+
     override func viewWillAppear(_ animated: Bool) {
         //Disabling Stop Recording button at the beginning
         stopRecordingButton.isEnabled = false
@@ -30,19 +30,19 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         recordingLabel.text = "Recording in progress"
         startRecordingButton.isEnabled = false
         stopRecordingButton.isEnabled = true
-        
+
         //Getting directory where the recorded audio file is going to be written
         let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
         let recordingName = "recordedVoice.wav"
-        
+
         //Combining directory and filename to form a complete URL
         let pathArray = [dirPath, recordingName]
         let filePath = URL(string: pathArray.joined(separator: "/"))
-        
+
         //Getting audio session that provides access to the hardware
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
-        
+
         //Setting up audio recorder and starting the actual recording
         try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
         audioRecorder.delegate = self
@@ -55,19 +55,19 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         recordingLabel.text = "Tap to record"
         startRecordingButton.isEnabled = true
         stopRecordingButton.isEnabled = false
-        
+
         if audioRecorder.isRecording {
             audioRecorder.stop()
             let audioSession = AVAudioSession.sharedInstance()
             try! audioSession.setActive(false)
         }
     }
-    
+
     /**
      This function is called when the audio recording and file saving processes are finished.
     */
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-        
+
         //If everything went well, we transition to our audio player view using performSegue() with identifier
         if flag {
             performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
@@ -75,7 +75,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
             print("File recording or saving failed")
         }
     }
-    
+
     /**
      This function sets up some values before leaving this ViewController bases on what segue triggered this action.
      */
